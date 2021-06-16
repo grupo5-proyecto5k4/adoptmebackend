6615d1941b86e6bc8d1327533c8b7bc88997f234
grupo5-proyecto5k4 <tamara.doffo@ross.com.ar>
2021-06-16 19:42:54 -0300

    test

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const user = require('./Logica/usuarios.js')
+const port = process.env.PORT || 8000; //borrar - heroku
+
 app.use(express.json())
 app.use('/api/usuario/', user)
 //app.use('/api/user/', user)
 //app.use('/api/company/', company)
 //app.use('/api/sale/', sale)
 //app.use('/api/auth/', auth)
-const port = process.env.PORT || 3003
-app.listen(port, ()=> console.log('Escuchando Puerto: ' + port))
+//const port = process.env.PORT || 3003
+//app.listen(port, ()=> console.log('Escuchando Puerto: ' + port))
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/BD_ADOPTME', {useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true})
     .then(()=> console.log('Conectado correctamente a MongoDB'))
     .catch(()=> console.log('Error al conectarse a MongoDB'));
 
-    
+server.listen(port, () => { //borrar heroku
+    console.log("App is running on port " + port);
+});
  //.connect (process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true,}).
 //then (() => console.log ('DB Connected!'))
 //.catch (err => {
