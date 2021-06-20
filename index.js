cca9f2ff98a2e31ff4539219053c953127c72f67
Evangelina Pereyra <evanpereyra@gmail.com>
2021-06-19 21:17:13 -0300

    cambio08

--- index.js
+++ index.js
@@ -18,12 +18,22 @@ const port = process.env.PORT || 3003
 app.listen(port, ()=> console.log('Escuchando Puerto: ' + port))
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/BD_ADOPTME', {useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true})
-    .then(()=> console.log('Conectado correctamente a MongoDB'))
-    .catch(()=> console.log('Error al conectarse a MongoDB'));
+mongoose.connect('mongodb+srv://epereyra:pass1234@cluster0.hhsnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+ {useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true})
+   .then(()=> console.log('Conectado correctamente a MongoDB'))
+   .catch(()=> console.log('Error al conectarse a MongoDB'));
 
  //.connect (process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true,}).
 //then (() => console.log ('DB Connected!'))
 //.catch (err => {
 //console.log ( DB Connection Error: ${err.message});
 //});   'mongodb://localhost:27017/BD_ADOPTME'
+
+/* const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://epereyra:pass1234@cluster0.hhsnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+}); */
