296456a11b6429f54b0a7389543ef6915eb114b5
grupo5-proyecto5k4 <tamara.doffo@ross.com.ar>
2021-07-26 20:12:25 -0300

    .

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const estados = require('./Logica/estados.js')
 const animal = require('./Logica/animal.js')
 //const foto = requiere('./modelos/foto') //la constante que trabaja en la base de datos para consultas, etcs
 
+
+app.use(multer({storage}.single('imagen'))); //tiene que tener el nombre imagen en el form del front
+app.use(express.urlencoded({extended:false}));
 app.use(express.json())
 app.use('/', user, estados)
 app.use('/estados/', estados)
@@ -22,7 +25,7 @@ if( process.env.NODE_ENV !=  'production'){
     dotenv.config();
 }
 
-app.use(multer({storage}.single('imagen'))); //tiene que tener el nombre imagen en el form del front
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_URL,
