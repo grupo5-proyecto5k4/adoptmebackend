1dcb352677434ddc836a042cbe01f47fcde6d706
grupo5-proyecto5k4 <tamara.doffo@ross.com.ar>
2021-07-26 21:24:54 -0300

    .

--- Logica/foto.js
+++ Logica/foto.js
@@ -22,28 +22,60 @@ router.get('/imagen', async (req,res) => {
     res.send(foto);
 });
 
-router.post('/imagen/add', async (req,res) => {
-   const {title, description} = req.body;
-   console.log(req.file); //informacion de la imagen
-   console.log(req.body); // datos cargados sobre la imagen titulo, etcs.
-   const result = await cloudinary.v2.uploader.upload(req.file.path); // toma el path de la foto que nos cargan desde el frontend
-   console.log(result)
-   const newFoto = new Foto ({
-       titulo,
-       descripcion,
-       imagenURL: result.url, // la url que guardo cuando cloudinary me sube la imagen
-       public_id: result.públic_id
-   })
-   await newFoto.save();
-   await fs.unlik(req.file.path);
-      let foto = new Foto({
-        titulo: req.body.titulo,
-        descripcion: req.body.descripcion,
-        public_id: req.body.public_id
-    })
-    result = await foto.save()
-    const jwtToken = foto.generateJWT()
-    {res.send('recibido ok');
-}
+// router.post('/imagen/add', async (req,res) => {
+//    const {title, description} = req.body;
+//    console.log(req.file); //informacion de la imagen
+//    console.log(req.body); // datos cargados sobre la imagen titulo, etcs.
+//    const result = await cloudinary.v2.uploader.upload(req.file.path); // toma el path de la foto que nos cargan desde el frontend
+//    console.log(result)
+//    const newFoto = new Foto ({
+//        titulo,
+//        descripcion,
+//        imagenURL: result.url, // la url que guardo cuando cloudinary me sube la imagen
+//        public_id: result.públic_id
+//    })
+//    await newFoto.save();
+//    await fs.unlik(req.file.path);
+//       let foto = new Foto({
+//         titulo: req.body.titulo,
+//         descripcion: req.body.descripcion,
+//         public_id: req.body.public_id
+//     })
+//     result = await foto.save()
+//     const jwtToken = foto.generateJWT()
+//     {res.send('recibido ok');
+// }
+// });
+const multer = require('multer')({
+    dest: '/home/tamara/Documentos/Personal/Tesis/Heroku/adoptmebackend/public/files'
+})
+
+const fs = require('fs')
+const path = require('path')
+router.post('/uploadimg', [multer.single('attachment')], (req, res, next) => {
+    console.log(req.file)
+    if (req.file.length == 0) {
+        responseb.error = true;
+        responseb.mensaje = 'Ingrese una imagen';
+        responseb.codigo = 400;
+        res.status(400).send(responseb);
+
+    } else {
+        if (req.file.mimetype.indexOf('image') >= 0) {
+            let { fileName } = storeWithOriginalName(req.file)
+            responseb.error = true;
+            responseb.mensaje = fileName;
+            responseb.codigo = 200;
+            res.status(200).send(responseb);
+
+        } else {
+            responseb.error = true;
+            responseb.mensaje = 'Ingrese una imagen';
+            responseb.codigo = 400;
+            res.status(400).send(responseb);
+        }
+
+
+    }
 });
 module.exports = router;
