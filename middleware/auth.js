8b6b9f764da83fdb51df5e41db3ecfc1941ce8a4
Evangelina Pereyra <evanpereyra@gmail.com>
2021-08-22 19:08:21 -0300

    cambio220801

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken')
 
 function auth(req, res, next){
-    const jwtToken = req.header('auth-token')
     
+    const jwtToken = req.header('auth-token')
+    console.log ("token ", jwtToken)
     if(!jwtToken) return res.status(401).send('Acceso Denegado. Necesitamos un token valido')
-
+  
     try{
         const payload = jwt.verify(jwtToken, process.env.SECRET_KEY_JWT)
         
