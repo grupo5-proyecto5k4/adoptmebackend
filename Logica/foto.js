5f6633fbfd685b3550935108a3cae67e1b974ea8
Evangelina Pereyra <evanpereyra@gmail.com>
2021-09-02 19:35:04 -0300

    cambio020909

--- Logica/foto.js
+++ Logica/foto.js
@@ -38,7 +38,7 @@ router.post('/imagen/add', async (req,res) => {
     console.log ("trae algo req.file",req.file)
     console.log (`./${req.file.path}`)
     if (!req.file) res.sendStatus(400).json({error: 'Error, no llegamos'})
-    const result = await cloudinary.uplouder.upload(`./${req.file.path}`)
+    const result = await cloudinary.uploader.upload(`./${req.file.path}.jpg`)
     newFoto = new Foto ({
         titulo: req.body.titulo,
         descripcion: req.body.descripcion,
@@ -47,7 +47,7 @@ router.post('/imagen/add', async (req,res) => {
         id_Animal: req.body.id_Animal
     })
     let resultado = await newFoto.save()
-    await fs.unlink(req.file.path)
+    await fs.unlink(`./${req.file.path}.jpg`)
     if (!resultado) res.sendStatus(400).json({error: 'Error, no llegamos'})
     res.sendStatus(400).json({mensaje: 'Se grabo correctamente'})
        
