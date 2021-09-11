0351e7fb90f4c9d1e62014ab1191f0cc8e78d5df
Evangelina Pereyra <evanpereyra@gmail.com>
2021-09-11 16:21:39 -0300

    cambio110900

--- modelos/vacuna.js
+++ modelos/vacuna.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken')
 const mongosee = require('mongoose')
+const { ObjectId } = require('mongodb');
 
 const vacunaSchema  = new mongosee.Schema({
   nombre: {type: String},
   cantidadDosis: {type: Number},
   fechaCreacion: {type: Date, default: Date.now},
   fechaModificacion:{type: Date, default: Date.now},
-  id_Animal: ObjectId
+  id_Animal:  ObjectId
 });
   
 vacunaSchema.methods.generateJWT = function(){
