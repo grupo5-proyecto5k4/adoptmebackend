75ae6cdb43fe62b20aca59446b9e7e28680faf6c
grupo5-proyecto5k4 <tamara.doffo@ross.com.ar>
2021-09-18 13:00:43 -0300

    .

--- Logica/vacuna.js
+++ Logica/vacuna.js
@@ -25,7 +25,7 @@ router.post('/vacuna', async function(req, res) {
             //cantidadDosis: Vacuna[index].cantidadDosis,
             //id_Animal: id
         //})
-    for (let index = 0;  req.body > index;  ++index ) {
+    for (let index = 0;  req.length > index;  ++index ) {
             let vac = new Vacuna({
                 nombreVacuna : req.body.nombreVacuna,
                 cantidadDosis: req.body.cantidadDosis,
