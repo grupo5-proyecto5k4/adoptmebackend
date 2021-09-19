ea7184919cee01c34e24a85f2f8a5037d24971ce
grupo5-proyecto5k4 <tamara.doffo@ross.com.ar>
2021-09-19 17:51:48 -0300

    .

--- Logica/usuarios.js
+++ Logica/usuarios.js
@@ -207,9 +207,11 @@ router.get('/usuarios/:estados', auth, async(req, res)=>{
     if (!estados) return res.status(404).json({error: 'El estado es invalido'})
      
     let users = await User.find({idEstado : estados.id_estado, tipoUsuario: 1})
-    if(users.length == 0) return res.status(404).json({error: 'No hemos encontrado un Usuarios en ese estado'})
+    let usersCentro = await usersCentro.find({idEstado : estados.id_estado, tipoUsuario: 2}) //
+    if(users.length == 0 || usersCentro == 2) return res.status(404).json({error: 'No hemos encontrado un Usuarios en ese estado'})
     
     res.send(users)
+    res.send(usersCentro)
 });
 
 
