e514b34667e57ec6319c9b9c67da4cd773c90d0b
grupo5-proyecto5k4 <tamara.doffo@ross.com.ar>
2021-10-08 13:51:20 -0300

    .

--- Logica/animal.js
+++ Logica/animal.js
@@ -100,15 +100,15 @@ router.get('/respestados/:responestados', auth, async(req, res)=>{
 
 //Filtrar mascota de un determinado estado ( x resp) y aplicar filtro: sexo, tipo animal(perro-gato)
 //Tamaño, centro rescatista, barrio/zona
-router.get('/filtrosMascota/:estadoAnimal/:sexoAnimal/:tipoAnimal', auth, async(req, res)=>{
+router.get('/filtrosMascota/:estadoAnimal/:sexoAnimal/:tipoAnimal/:tamañoAnimal', auth, async(req, res)=>{
     let nuevoEstado = req.params.estadoAnimal.replace(/_/g, " ")
     let nuevousuario = req.user.user
     let nuevoSexo = req.params.sexoAnimal
     let nuevoTipoAnimal = req.params.tipoAnimal
-    //let nuevoTamañoAnimal = req.params.tamañoAnimal
+    let nuevoTamañoAnimal = req.params.tamañoAnimal
     //let nuevoBarrioAnimal = req.params.barrioAnimal
     if (nuevousuario.tipoUsuario != 0){
-        let animalDevuelto = await Animal.find({estado : nuevoEstado, sexo: nuevoSexo, tipoMascota : nuevoTipoAnimal})
+        let animalDevuelto = await Animal.find({estado : nuevoEstado, sexo: nuevoSexo, tipoMascota : nuevoTipoAnimal, tamañoFinal : nuevoTamañoAnimal })
         if (animalDevuelto.length == 0) return res.status(400).json({mesage:'No existen animales que coincidan con los filtros deseados'})
         res.send(animalDevuelto)
     }
