6e3b5b98b18dd127bfc691734a2a4dc30601f144
grupo5-proyecto5k4 <tamara.doffo@ross.com.ar>
2021-10-11 22:45:06 -0300

    .

--- Logica/animal.js
+++ Logica/animal.js
@@ -129,14 +129,10 @@ router.get('/filtrosMascota/filtroAnimalCentroResc', async(req, res)=>{
 
     for (let i = 0 ; i < animalDevuelto.length ; i ++ ){
             const filter4 = {} 
-            //filter4._id = animalDevuelto[i].responsableId
-            //filter4.tipoUsuario = 2
-            //if(req.body.nombres)filter4.nombres = req.body.nombres
-            //if(req.body.Direccion)filter4.Direccion = req.body.Direccion
+            
             filter4._id = animalDevuelto[i].responsableId
-            //console.log(filter4)
             let usuarioDevueltoNew = await Usuario.findById(filter4)
-            //console.log(usuarioDevueltoNew)
+           
             if(!usuarioDevueltoNew) continue
             if(usuarioDevueltoNew.nombres != req.body.nombres && req.body.nombres) continue
             if(usuarioDevueltoNew.Direccion.barrio != req.body.barrio && req.body.barrio) continue
@@ -148,7 +144,6 @@ router.get('/filtrosMascota/filtroAnimalCentroResc', async(req, res)=>{
             filtroDevuelto.push(nuevoArreglo)    
             };
             res.send(filtroDevuelto)
-            
 })
 module.exports = router;
 
