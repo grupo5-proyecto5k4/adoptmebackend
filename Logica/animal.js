0d9673b2a1caa52ba7c97de00eb7ef2df804ed5a
grupo5-proyecto5k4 <81835477+grupo5-proyecto5k4@users.noreply.github.com>
2021-10-15 00:00:24 -0300

    se cambia body por query

--- Logica/animal.js
+++ Logica/animal.js
@@ -4,7 +4,7 @@ const express = require('express')
 const Animal = require('../modelos/animal.js')
 const jwt = require('jsonwebtoken')
 const router = express.Router()
-const {check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const auth = require('../middleware/auth.js')
 const { ObjectId } = require('mongodb');
 const Foto = require('../modelos/foto.js')
@@ -18,44 +18,44 @@ const Usuario = require('../modelos/usuarios.js')
 
 
 cloudinary.config({
-     cloud_name: process.env.cloudname,
-     api_key: process.env.apikey,
-     api_secret: process.env.apisecret
- })
+    cloud_name: process.env.cloudname,
+    api_key: process.env.apikey,
+    api_secret: process.env.apisecret
+})
 
 
 //Buscar un animal por un determinado id
-router.get('/buscar', async function(req, res) {
+router.get('/buscar', async function (req, res) {
     console.log('llega')
 
-    const animal =  await Animal.find()
+    const animal = await Animal.find()
     console.log(animal)
     res.send(animal)
 })
 
-router.post('/animal', auth,  async function(req, res) {
+router.post('/animal', auth, async function (req, res) {
     let userAux = req.user.user
-    let castrado = true 
-    if (req.body.castrado == 2) castrado = false 
+    let castrado = true
+    if (req.body.castrado == 2) castrado = false
     let estado = " "
-    switch(req.body.estado){
-        case 7 : 
-           estado = "Disponible Adopción";
-           break;
-        case 8 : 
-           estado = "Disponible Adopción y Provisorio";
-           break;
-        case 9 : 
-           estado = "Disponible Provisorio";
-           break;
-        default: 
-           estado = "Disponible Adopción y Provisorio";          
+    switch (req.body.estado) {
+        case 7:
+            estado = "Disponible Adopción";
+            break;
+        case 8:
+            estado = "Disponible Adopción y Provisorio";
+            break;
+        case 9:
+            estado = "Disponible Provisorio";
+            break;
+        default:
+            estado = "Disponible Adopción y Provisorio";
     }
-      
-  
+
+
     let animal = new Animal({
         tipoMascota: req.body.tipoMascota,
-        nombreMascota : req.body.nombreMascota,
+        nombreMascota: req.body.nombreMascota,
         fechaAlta: req.body.fechaAlta,
         fechaModificacion: req.body.fechaModificacion,
         tamañoFinal: req.body.tamañoFinal,
@@ -71,94 +71,99 @@ router.post('/animal', auth,  async function(req, res) {
         conductaGatos: req.body.conductaGatos,
         descripcion: req.body.descripcion,
         foto: req.body.foto
- })
-   
-const result = await animal.save()
+    })
 
-const jwtToken = result.generateJWT()
- 
-console.log (result)
+    const result = await animal.save()
 
-res.status(201).json({id_Animal: result._id})
+    const jwtToken = result.generateJWT()
+
+    console.log(result)
+
+    res.status(201).json({ id_Animal: result._id })
 });
 
 // filtrar mascotas segun su estado
-router.get('/animal/:estados', async(req, res)=>{
+router.get('/animal/:estados', async (req, res) => {
     let nueva = req.params.estados.replace(/_/g, " ")
-    let animal = await Animal.find({estado : nueva}) 
-    if (animal.length == 0) return res.status(200).json({mesage:'[]'})
+    let animal = await Animal.find({ estado: nueva })
+    if (animal.length == 0) return res.status(200).json({ mesage: '[]' })
     res.send(animal)
 });
 
 // filtrar mascotas segun su estado y segun el id del responsable
-router.get('/respestados/:responestados', auth, async(req, res)=>{
+router.get('/respestados/:responestados', auth, async (req, res) => {
     let nueva = req.params.responestados.replace(/_/g, " ")
     let userAux = req.user.user
-    let animal = await Animal.find({responsableId : userAux._id, estado : nueva }) 
-        
-    if (animal.length == 0) return res.status(200).json({mesage:'[]'})
+    let animal = await Animal.find({ responsableId: userAux._id, estado: nueva })
+
+    if (animal.length == 0) return res.status(200).json({ mesage: '[]' })
     res.send(animal)
 });
 
 //Traer todas las mascotas que cumplen con los filtros, en gral, no responden a un det usuario
-router.get('/filtrosMascota/filtroAnimal', auth, async(req, res)=>{
+router.get('/filtrosMascota/filtroAnimal', auth, async (req, res) => {
     const filter = {}
-    if(req.body.estado)filter.estado = req.body.estado 
-    if(req.body.sexo) filter.sexo = req.body.sexo
-    if(req.body.tamañoFinal) filter.tamanoFinal = req.body.tamanoFinal
-    if(req.body.tipoAnimal) filter.tipoAnimal = req.body.tipoAnimal
+    if (req.body.estado) filter.estado = req.body.estado
+    if (req.body.sexo) filter.sexo = req.body.sexo
+    if (req.body.tamañoFinal) filter.tamanoFinal = req.body.tamanoFinal
+    if (req.body.tipoAnimal) filter.tipoAnimal = req.body.tipoAnimal
     let animalDevuelto = await Animal.find(filter)
-    if (animalDevuelto.length == 0) return res.status(400).json({mesage:'No existen animales que coincidan con los filtros deseados'})
+    if (animalDevuelto.length == 0) return res.status(400).json({ mesage: 'No existen animales que coincidan con los filtros deseados' })
     res.send(animalDevuelto)
 })
 
 //Filtrar las mascotas por los filtros del item anterior y ademas filtrar en funcion
 //de los datos del centro recatista (barrio del centro y nombre del centro)
-router.get('/filtrosMascota/filtroAnimalCentroResc', async(req, res)=>{
+router.get('/filtrosMascota/filtroAnimalCentroResc', async (req, res) => {
     const filter2 = {}
     let filtroDevuelto = []
-    if(req.body.estado)filter2.estado = req.body.estado 
-    if(req.body.sexo) filter2.sexo = req.body.sexo
-    if(req.body.tamañoFinal) filter2.tamañoFinal = req.body.tamañoFinal
-    if(req.body.tipoAnimal) filter2.tipoAnimal = req.body.tipoAnimal
-    
+    const { estado, sexo, tamañoFinal, tipoAnimal } = req.query;
+
+    if (estado) filter2.estado = estado;
+    if (sexo) filter2.sexo = sexo;
+    if (tamañoFinal) filter2.tamañoFinal = tamañoFinal;
+    if (tipoAnimal) filter2.tipoAnimal = tipoAnimal;
+
     let animalDevuelto = await Animal.find(filter2)
-    if(animalDevuelto.length == 0) return res.status(400).json({mesage:'Error'})
+    if (animalDevuelto.length == 0) return res.status(400).json({ mesage: 'Error' })
     console.log(animalDevuelto)
-    for (let i = 0 ; i < animalDevuelto.length ; i ++ ){
-            const filter4 = {} 
-            
-            filter4._id = animalDevuelto[i].responsableId
-            let usuarioDevueltoNew = await Usuario.findById(filter4)
-           
-            if(!usuarioDevueltoNew) continue
-            if(usuarioDevueltoNew.nombres != req.body.nombres && req.body.nombres) continue
-            if(usuarioDevueltoNew.Direccion.barrio != req.body.barrio && req.body.barrio) continue
-            var nuevoArreglo = {
-                Animal: animalDevuelto[i], 
-                }
-            filtroDevuelto.push(nuevoArreglo.Animal)    
-            };
-            res.send(filtroDevuelto)
+    for (let i = 0; i < animalDevuelto.length; i++) {
+        const filter4 = {}
+
+        filter4._id = animalDevuelto[i].responsableId
+        let usuarioDevueltoNew = await Usuario.findById(filter4)
+
+        if (!usuarioDevueltoNew) continue
+        if (usuarioDevueltoNew.nombres != req.query.nombres && req.query.nombres) continue
+        if (usuarioDevueltoNew.Direccion.barrio != req.query.barrio && req.query.barrio) continue
+        var nuevoArreglo = {
+            Animal: animalDevuelto[i],
+        }
+        filtroDevuelto.push(nuevoArreglo.Animal)
+    };
+    res.send(filtroDevuelto)
 })
 
 //Filtros de mascota segun el id de un determinado usuario
 
-router.get('/filtrosMascotaUsuario/test', async(req, res)=>{
+router.get('/filtrosMascotaUsuario/', async (req, res) => {
     const filter = {}
-    if(req.body.estado)filter.estado = req.body.estado 
-    if(req.body.sexo) filter.sexo = req.body.sexo
-    if(req.body.tamañoFinal) filter.tamañoFinal = req.body.tamañoFinal
-    if(req.body.tipoAnimal) filter.tipoAnimal = req.body.tipoAnimal
-    if(req.body.responsableId) filter.responsableId = req.body.responsableId
+
+    const { estado, sexo, tamañoFinal, tipoAnimal, responsableId } = req.query;
+
+    if (estado) filter.estado = estado;
+    if (sexo) filter.sexo = sexo;
+    if (tamañoFinal) filter.tamañoFinal = tamañoFinal;
+    if (tipoAnimal) filter.tipoAnimal = tipoAnimal;
+    if (responsableId) filter.responsableId = responsableId;
     let animalDevuelto = await Animal.find(filter)
-    if (animalDevuelto.length == 0) return res.status(400).json({mesage:'No existen animales que coincidan con los filtros deseados'})
+    if (animalDevuelto.length == 0) return res.status(400).json({ mesage: 'No existen animales que coincidan con los filtros deseados' })
     res.send(animalDevuelto)
 })
 
 module.exports = router;
 
 
-    
+
 
 
