504745ab7e3ca42c39a0398ebc15560f55706b2d
Evangelina Pereyra <evanpereyra@gmail.com>
2021-11-14 20:13:52 -0300

    cambio141121 - Reportes

--- fecha.js
+++ fecha.js
@@ -1,4 +1,4 @@
 exports.ahora = function(){ return new Date(Date.now()- (3*3600*1000)).toISOString()};
 exports.resta = function(X){ return new Date(Date.now()- ((X+3)*3600*1000)).toISOString()};
 exports.sumar = function(X){ return new Date(Date.now()- ((3-X)*3600*1000)).toISOString()};
-exports.redondear = function(X){ if(X > 0 && X < 1 ) X = 1;  return X};
+exports.redondear = function(X){ if(X >= 0 && X < 1 ) X = 1;  return X};
